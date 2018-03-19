5cf7c1ce5da10e84ea9c92e92b4d1b6226c44555
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-03-19 13:16:38 +1100

    update num of paper for fields

--- python/count_citations.py
+++ python/count_citations.py
@@ -6,27 +6,35 @@ from collections import Counter
 from operator import itemgetter
 from academic_search import *
 
-def read_data():
-    datapath = "../data/0804-filtered.csv"
-    reader = csv.reader(open(datapath), delimiter=',', skipinitialspace=True)
-    next(reader) # skip the first line
-    data = dict((r[0], {
-        "Type": r[1],
-        "Authors": r[2],
-        "Title": r[3],
-        "Year": r[4],
-        "Outlet": r[5]
-    }) for r in reader if len(r) > 6 and r[0] != "")
-    return data
-
 if __name__ == "__main__":
-    data = read_data()
-    for p,v in data.items():
-        title = v["Title"]
-        try:
-            paper = get_paperinfo_from_title(title.strip())
-            print(p, paper["entities"][0]["Id"])
-        except Exception as e:
-            print(p, "error", title)
-            #print(e)
+    # field_name = "deep learning"
+    # field_name = "compiler"
+    # field_name = "assembly language"
+    field_name = "flash memory"
+    # field_name = "computer graphics"
+    print("field yearmap", field_name)
+
+    maxrange = 1000
+    rangebin = 50
+    result = {}
+    citeranges = range(rangebin, maxrange, rangebin)
+    for c in citeranges:
+        citation = [c, c+rangebin]
+        if c + rangebin == maxrange:
+            citation = [c, 1000000]
+        data = get_papers_from_field_of_study(field_name, citation)
+        numpaper = 0
+        numcite = 0
+        if "entities" in data:
+            numpaper = len(data["entities"])
+            numcite = sum([p["CC"] for p in data["entities"]])
+        print(citation, numpaper, numcite)
+        result[c] = {
+            "numpaper": numpaper,
+            "numcite": numcite,
+            "avgcite": numcite/numpaper if numcite > 0 else 0
+        }
         time.sleep(1)
+
+    with open('{}_stat.txt'.format(field_name), 'w') as outfile:
+        json.dump(result, outfile)
