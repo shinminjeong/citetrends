5c2e4c4c47c92a4944bcba2692eed353f717cd34
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-03-21 16:58:20 +1100

    plot yearmap for nobel prize winners

--- python/field_trends.py
+++ python/field_trends.py
@@ -30,7 +30,7 @@ def create_papers_yearmap(papers):
     yeardata = get_years_from_papers(list(set(cite_paper_ids)))
     yearmap = {d[0]["CellID"]:d[0]["PublishYear"] for d in yeardata["Results"]}
 
-    bins = range(2000, 2016)
+    bins = range(1940, 2019)
     result = {year:{y:0 for y in bins} for year in bins}
     for pubyear in bins:
         if pubyear in cite_year_map:
@@ -44,19 +44,28 @@ if __name__ == "__main__":
     # field_name = "compiler"
     # field_name = "assembly language"
     # field_name = "flash memory"
-    field_name = "computer graphics"
-    citation = [300, 1000]
-    data = get_papers_from_field_of_study(field_name, citation)
-    papers = {p["Id"]:p for p in data["entities"]}
+    # field_name = "computer graphics"
+    # citation = [300, 1000]
+    # data = get_papers_from_field_of_study(field_name, citation)
+    # papers = {p["Id"]:p for p in data["entities"]}
 
-    print("field yearmap", field_name)
+    # print("field yearmap", field_name)
     result = {}
-    citeyear, pubyear = create_papers_yearmap(papers)
+    # citeyear, pubyear = create_papers_yearmap(papers)
+    # arthor_name = "brian p schmidt"
+    # arthor_name = "frank wilczek"
+    # arthor_name = "george f smoot"
+    # arthor_name = "yoichiro nambu"
+    arthor_name = "peter w higgs"
+    citeyear, pubyear = create_author_yearmap(arthor_name)
+
     for k in citeyear.keys():
         result[k] = {
             "pub": len(pubyear[k]) if k in pubyear else 0,
             "cite": citeyear[k]
         }
     print(result)
-    with open('computer_graphics/{}_{}.txt'.format(citation[0], citation[1]), 'w') as outfile:
+    # with open('computer_graphics/{}_{}.txt'.format(citation[0], citation[1]), 'w') as outfile:
+    #     json.dump(result, outfile)
+    with open('{}.txt'.format(arthor_name), 'w') as outfile:
         json.dump(result, outfile)
