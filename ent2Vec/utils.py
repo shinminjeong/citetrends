cd4a632de6f7da9c6e5ff94b623513b13495b8aa
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-10-08 17:12:11 +1100

    revert time measures

--- ent2Vec/utils.py
+++ ent2Vec/utils.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 from multiprocessing import Pool
 
-ES_HOSTS = "130.56.248.7:9200"
+ES_HOSTS = "130.56.249.107:9200"
 query_matchall = {
     "query": {
         "match_all": {}
@@ -100,59 +100,88 @@ def search_pref(plist):
         refdict[ref["_source"]["PaperId"]].append(ref["_source"]["PaperReferenceId"])
     return refdict
 
-DEBUG = True
 def get_paper_ref(id, type):
-    t1 = datetime.now()
     res = search_papers(id, type)
-    t2 = datetime.now()
-    if DEBUG:
-        print((t2-t1).total_seconds(),
-        "Get paper ids from a venue (search papers) or from an author (search paa and papers)")
-
     yearMap = {int(r["_id"]):int(r["_source"]["Year"]) for r in res}
     paperids = list(yearMap.keys())
 
-    num_threads = 1
+    num_threads = 8
     p = Pool(num_threads)
+    ref_result = p.apply_async(search_pref, [paperids]).get()
 
-    t1 = datetime.now()
-    # ref_result = p.map_async(search_pref_indv, paperids).get()
-    ref_result = p.map_async(search_pref, [paperids]).get()
-    t2 = datetime.now()
-    if DEBUG:
-        print((t2-t1).total_seconds(),
-        "Get paper references from paper ids (search pref). # of threads = {}".format(num_threads))
+    confMap = dict()
+    conf_result = p.map(get_conf_from_pids, ref_result.values())
+    for group in conf_result:
+        for r in group:
+            if "ConferenceSeriesId" in r["_source"]:
+                confMap[int(r["_id"])] = int(r["_source"]["ConferenceSeriesId"])
+            if "JournalId" in r["_source"]:
+                confMap[int(r["_id"])] = int(r["_source"]["JournalId"])
+    """
+    return dictionary
+    {paperid: {"Year": year, "References": [list of venue ids]}}
+    """
+    paper_ref_dict = dict()
+    for p in paperids:
+        paper_ref_dict[p] = {"Year": yearMap[p], "References":[]}
+        for ref in ref_result[p]:
+            if ref in confMap:
+                paper_ref_dict[p]["References"].append(confMap[ref])
+    return paper_ref_dict
 
-    # confMap = dict()
-    # aggr_list = []
-    # for refs in ref_result:
-    #     for l in refs.values():
-    #         aggr_list.extend(l)
-    # t1 = datetime.now()
-    # conf_result = p.map_async(get_conf_from_pids, [list(set(aggr_list))]).get()
-    # t2 = datetime.now()
-    # if DEBUG:
-    #     print((t2-t1).total_seconds(),
-    #     "Get venue of {} paper references (search papers). # of threads = {}".format(len(aggr_list), num_threads))
-    #
-    # for group in conf_result:
-    #     for r in group:
-    #         if "ConferenceSeriesId" in r["_source"]:
-    #             confMap[int(r["_id"])] = int(r["_source"]["ConferenceSeriesId"])
-    #         if "JournalId" in r["_source"]:
-    #             confMap[int(r["_id"])] = int(r["_source"]["JournalId"])
-    #
-    # """
-    # return dictionary
-    # {paperid: {"Year": year, "References": [list of venue ids]}}
-    # """
-    # paper_ref_dict = dict()
-    # for p in paperids:
-    #     paper_ref_dict[p] = {"Year": yearMap[p], "References":[]}
-    #     for ref in ref_result[p]:
-    #         if ref in confMap:
-    #             paper_ref_dict[p]["References"].append(confMap[ref])
-    # return paper_ref_dict
+# DEBUG = False
+# def get_paper_ref(id, type):
+#     t1 = datetime.now()
+#     res = search_papers(id, type)
+#     t2 = datetime.now()
+#     if DEBUG:
+#         print((t2-t1).total_seconds(),
+#         "Get paper ids from a venue (search papers) or from an author (search paa and papers)")
+#
+#     yearMap = {int(r["_id"]):int(r["_source"]["Year"]) for r in res}
+#     paperids = list(yearMap.keys())
+#
+#     num_threads = 1
+#     p = Pool(num_threads)
+#
+#     t1 = datetime.now()
+#     # ref_result = p.map_async(search_pref_indv, paperids).get()
+#     ref_result = p.map_async(search_pref, [paperids]).get()
+#     t2 = datetime.now()
+#     if DEBUG:
+#         print((t2-t1).total_seconds(),
+#         "Get paper references from paper ids (search pref). # of threads = {}".format(num_threads))
+#
+#     confMap = dict()
+#     aggr_list = []
+#     for refs in ref_result:
+#         for l in refs.values():
+#             aggr_list.extend(l)
+#     t1 = datetime.now()
+#     conf_result = p.map_async(get_conf_from_pids, [list(set(aggr_list))]).get()
+#     t2 = datetime.now()
+#     if DEBUG:
+#         print((t2-t1).total_seconds(),
+#         "Get venue of {} paper references (search papers). # of threads = {}".format(len(aggr_list), num_threads))
+#
+#     for group in conf_result:
+#         for r in group:
+#             if "ConferenceSeriesId" in r["_source"]:
+#                 confMap[int(r["_id"])] = int(r["_source"]["ConferenceSeriesId"])
+#             if "JournalId" in r["_source"]:
+#                 confMap[int(r["_id"])] = int(r["_source"]["JournalId"])
+#
+#     """
+#     return dictionary
+#     {paperid: {"Year": year, "References": [list of venue ids]}}
+#     """
+#     paper_ref_dict = dict()
+#     for p in paperids:
+#         paper_ref_dict[p] = {"Year": yearMap[p], "References":[]}
+#         for ref in ref_result[p]:
+#             if ref in confMap:
+#                 paper_ref_dict[p]["References"].append(confMap[ref])
+#     return paper_ref_dict
 
 
 if __name__ == '__main__':
