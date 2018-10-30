22cf859899f1d2952637568cf8a13820378639c3
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-10-30 16:00:01 +1100

    svgjs drawing of bovs

---
+++ canvas/js/drawing.js
@@ -0,0 +1,115 @@
+var width = $(window).width(), height = $(window).height()-100;
+var legend_margin = 200;
+var draw = SVG('drawing').size(width, height);
+var bbox = [0, 0, 0, 0] // (x_min, x_max, y_min, y_max)
+var nsize = 6, nsizeb = 10;
+
+var colors = ["#065143", "#129490",
+  "#70B77E", "#E0A890", "#CE1483", "#fe4a49",
+  "#A2DDE1", "#FCAB10", "#ff9234", "#fd0054",
+  "#0b409c", "#5e227f"]
+var glist = [];
+var every_nodes = {};
+var every_nodes_t = {};
+
+var filename = GetURLParameter("input");
+console.log(filename)
+
+$.getJSON("http://127.0.0.1:8080/data/"+filename, function( data ) {
+    var groups = new Set();
+    for (var key in data) {
+      // calculating boundary box
+      bbox[0] = Math.min(bbox[0], data[key][0])
+      bbox[1] = Math.max(bbox[1], data[key][0])
+      bbox[2] = Math.min(bbox[2], data[key][1])
+      bbox[3] = Math.max(bbox[3], data[key][1])
+      // save names of nodes
+      var name = key.split("_");
+      groups.add(name[0]);
+      every_nodes[name[0]] = [];
+      every_nodes_t[name[0]] = [];
+    }
+
+    glist = Array.from(groups); // list of names
+    var xd = bbox[1]-bbox[0],
+        yd = bbox[3]-bbox[2];
+    var xs = 1+(width-legend_margin)/xd, ys = 1+height/yd;
+
+    // draw circle for each point
+    for (var key in data) {
+      var name = key.split("_");
+      var gname = name[0], year = name[1];
+      var circle = draw.circle(nsize*2).id(gname)
+          .center(data[key][0]*xs+(width-legend_margin)/2, data[key][1]*ys+height/2)
+          .fill(colors[glist.indexOf(gname)]);
+      every_nodes[gname].push(circle);
+    }
+
+    // draw year for each point
+    for (var key in data) {
+      var name = key.split("_");
+      var gname = name[0], year = name[1];
+      var c_text = draw.text(year).id(year).fill("#000")
+          .move(data[key][0]*xs+(width-legend_margin)/2+10, data[key][1]*ys+height/2)
+          .attr("visibility", "hidden");
+      every_nodes_t[gname].push(c_text);
+    }
+
+    // draw legends
+    for (var gid in glist) {
+      var legend_rect = draw.rect(20,20).id(gid)
+          .fill(colors[gid]).move(width-legend_margin, gid*25+20);
+      var legend_text = draw.text(glist[gid]).id(gid)
+          .move(width-legend_margin+30, gid*25+20)
+          .fill("#000");
+
+      legend_rect.mouseover(function() { highlight_group(glist[this.node.id]) });
+      legend_text.mouseover(function() { highlight_group(glist[this.node.id]) });
+
+      legend_rect.mouseout(function() { reset_highlight() });
+      legend_text.mouseout(function() { reset_highlight() });
+    }
+
+});
+
+function highlight_group(gname) {
+  // console.log("highlight_group", gname);
+  dim_every_nodes();
+  for (var e in every_nodes[gname]) {
+    every_nodes[gname][e].attr("r", nsizeb).attr("opacity", 1)
+        .fill(colors[glist.indexOf(gname)]);
+    every_nodes_t[gname][e].attr("visibility", "visible");
+  }
+}
+
+function dim_every_nodes() {
+  for (var gname in every_nodes) {
+    for (var e in every_nodes[gname]) {
+      every_nodes[gname][e].attr("opacity", 0.6);
+    }
+  }
+}
+
+function reset_highlight() {
+  for (var gname in every_nodes) {
+    for (var e in every_nodes[gname]) {
+      every_nodes[gname][e].attr("r", nsize)
+          .attr("opacity", 1)
+          .fill(colors[glist.indexOf(gname)]);
+    }
+    for (var e in every_nodes_t[gname]) {
+      every_nodes_t[gname][e].attr("visibility", "hidden");
+    }
+  }
+}
+
+function GetURLParameter(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) {
+      return sParameterName[1];
+    }
+  }
+}
--- ent2Vec/utils.py
+++ ent2Vec/utils.py
@@ -1,7 +1,12 @@
-import os, json
+import os, json, itertools
+import numpy as np
+from collections import Counter
 from datetime import datetime
 from elasticsearch import Elasticsearch
 from multiprocessing import Pool
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 ES_HOSTS = "130.56.249.107:9200"
 query_matchall = {
@@ -70,6 +75,7 @@ def get_conf_from_pids(plist):
     }
     return query_es("papers", query)
 
+
 def search_pref_indv(paper):
     refdict = {paper:[]}
     query = {
@@ -129,61 +135,142 @@ def get_paper_ref(id, type):
                 paper_ref_dict[p]["References"].append(confMap[ref])
     return paper_ref_dict
 
-# DEBUG = False
-# def get_paper_ref(id, type):
-#     t1 = datetime.now()
-#     res = search_papers(id, type)
-#     t2 = datetime.now()
-#     if DEBUG:
-#         print((t2-t1).total_seconds(),
-#         "Get paper ids from a venue (search papers) or from an author (search paa and papers)")
-#
-#     yearMap = {int(r["_id"]):int(r["_source"]["Year"]) for r in res}
-#     paperids = list(yearMap.keys())
-#
-#     num_threads = 1
-#     p = Pool(num_threads)
-#
-#     t1 = datetime.now()
-#     # ref_result = p.map_async(search_pref_indv, paperids).get()
-#     ref_result = p.map_async(search_pref, [paperids]).get()
-#     t2 = datetime.now()
-#     if DEBUG:
-#         print((t2-t1).total_seconds(),
-#         "Get paper references from paper ids (search pref). # of threads = {}".format(num_threads))
-#
-#     confMap = dict()
-#     aggr_list = []
-#     for refs in ref_result:
-#         for l in refs.values():
-#             aggr_list.extend(l)
-#     t1 = datetime.now()
-#     conf_result = p.map_async(get_conf_from_pids, [list(set(aggr_list))]).get()
-#     t2 = datetime.now()
-#     if DEBUG:
-#         print((t2-t1).total_seconds(),
-#         "Get venue of {} paper references (search papers). # of threads = {}".format(len(aggr_list), num_threads))
-#
-#     for group in conf_result:
-#         for r in group:
-#             if "ConferenceSeriesId" in r["_source"]:
-#                 confMap[int(r["_id"])] = int(r["_source"]["ConferenceSeriesId"])
-#             if "JournalId" in r["_source"]:
-#                 confMap[int(r["_id"])] = int(r["_source"]["JournalId"])
-#
-#     """
-#     return dictionary
-#     {paperid: {"Year": year, "References": [list of venue ids]}}
-#     """
-#     paper_ref_dict = dict()
-#     for p in paperids:
-#         paper_ref_dict[p] = {"Year": yearMap[p], "References":[]}
-#         for ref in ref_result[p]:
-#             if ref in confMap:
-#                 paper_ref_dict[p]["References"].append(confMap[ref])
-#     return paper_ref_dict
+
+def save_as_file(filename, dictdata):
+    with open("../data/{}.json".format(filename), "w") as outfile:
+        json.dump(dictdata, outfile)
+
+
+def get_set_of_venues_by_year(res):
+    references = []
+    for p, r in res.items():
+        if "References" in r and "Year" in r:
+            references.append((r["Year"], r["References"]))
+    venues = {}
+    for y, ref in references:
+        if y not in venues:
+            venues[y] = []
+        venues[y].extend(ref)
+    return venues
+
+def aggr_venues(data):
+    venues = []
+    for y, ref in data.items():
+        venues.extend(ref)
+    return venues
+
+def get_vector(name, bov, author_venue, norm=False):
+    c = Counter(author_venue)
+    author_arr = [[float(c[b]) for b in bov]]
+    if norm:
+        return np.array(author_arr)/name_id_pairs[name]["numpaper"]
+    else:
+        return np.array(author_arr)
+
+name_id_pairs = {
+    # "steve_blackburn": {"id": 2146610949, "type":"author"},
+    # "kathryn_mckinley": {"id": 2115847858, "type":"author"},
+    # "james_bornholt": {"id": 2026265091, "type":"author"},
+    # "julian_dolby": {"id": 2780161863, "type":"author"},
+    # "perry_cheng": {"id": 2140441920, "type":"author"},
+    # "julian_mcauley": {"id": 2041520510, "type":"author"},
+    "ICWSM": {"id": 1124713781, "type":"conf"},
+    "POPL": {"id": 1160032607, "type":"conf"},
+    "PLDI": {"id": 1127352206, "type":"conf"},
+    "WSDM": {"id": 1120384002, "type":"conf"},
+    "CIKM": {"id": 1194094125, "type":"conf"},
+    "ISCA": {"id": 1131341566, "type":"conf"},
+    "MICRO": {"id": 1150919317, "type":"conf"},
+    "ASPLOS": {"id": 1174091362, "type":"conf"},
+    "OSDI": {"id": 1185109434, "type":"conf"},
+    "ICML": {"id": 1180662882, "type":"conf"},
+    "NIPS": {"id": 1127325140, "type":"conf"},
+    "OOPSLA": {"id": 1138732554, "type":"conf"},
+}
+
+def generate_data():
+    for cname, value in name_id_pairs.items():
+        data = get_paper_ref(value["id"], value["type"])
+        save_as_file(cname, data)
+
+
+def generate_bov():
+    conf_vectors = {}
+    for cname in name_id_pairs.keys():
+        with open("../data/{}.json".format(cname), "r") as infile:
+            data = json.load(infile)
+            name_id_pairs[cname]["numpaper"] = len(data)
+            conf_vectors[cname] = get_set_of_venues_by_year(data)
+    return conf_vectors
+
+def print_cos_similarity(vec):
+    for x,y in itertools.combinations(name_id_pairs.keys(), 2):
+        print(x, y, cosine_similarity(vec[x], vec[y]))
+
+
+def reduce_vec_pca(vec):
+    pca = PCA(n_components=2)
+    X = np.zeros((len(vec),number_of_venues))
+    for i, v in enumerate(vec.values()):
+        X[i] = v
+    pca.fit(X)
+    X_pca = pca.transform(X)
+    # print("original shape:   ", X.shape)
+    # print("transformed shape:", X_pca.shape)
+    result = {}
+    for i, k in enumerate(vec.keys()):
+        result[k] = X_pca[i].tolist()
+    return result
+
+def reduce_vec_tsne(vec, p):
+    tsne = TSNE(n_components=2, perplexity=p)
+    X = np.zeros((len(vec),number_of_venues))
+    for i, v in enumerate(vec.values()):
+        X[i] = v
+    X_tsne = tsne.fit_transform(X)
+    # print("original shape:   ", X.shape)
+    # print("transformed shape:", X_tsne.shape)
+    result = {}
+    for i, k in enumerate(vec.keys()):
+        result[k] = X_tsne[i].tolist()
+    return result
 
 
 if __name__ == '__main__':
-    get_paper_ref(1120384002, "conf")
-    # get_paper_ref(1127352206, "conf")
+    # generate_data()
+    data = generate_bov()
+
+    bag_of_venues = set()
+    sorted_list_bov = list()
+    for name, y_venues in data.items():
+        venues = aggr_venues(y_venues)
+        print("{}: len(papers)={}, len(venues)={}, len(set(venues))={}".format(name, name_id_pairs[name]["numpaper"], len(venues), len(set(venues))))
+        [bag_of_venues.add(v) for v in venues]
+    print("Total # of venues = ", len(bag_of_venues))
+    sorted_list_bov = list(bag_of_venues)
+    number_of_venues = len(sorted_list_bov)
+
+    ## generate bov for each entity
+    # vec = {}
+    # avg_vec = {}
+    # for name, y_venues in data.items():
+    #     vec[name] = get_vector(name, sorted_list_bov, aggr_venues(y_venues))
+    #     avg_vec[name] = get_vector(name, sorted_list_bov, aggr_venues(y_venues), True)
+    # print_cos_similarity(vec)
+    # print(reduce_vec_dim(vec))
+
+    ## generate year_bov for each entity
+    vec = {}
+    for name, y_venues in data.items():
+        for y, ref in y_venues.items():
+            vec["{}_{}".format(name, y)] = get_vector(name, sorted_list_bov, ref)
+    # print_cos_similarity(vec)
+    # reduce_vec = reduce_vec_pca(vec)
+    # print(reduce_vec)
+    # save_as_file("pca_reduce_vec", reduce_vec)
+
+    for i in [1,2,3]:
+        for p in [5, 10, 20, 40, 80]:
+            reduce_t_vec = reduce_vec_tsne(vec, p)
+            # print(reduce_vec)
+            save_as_file("tsne_vec_{}_{}".format(i, p), reduce_t_vec)
