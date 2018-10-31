8d76a8f1af104ad63e9e193ced5d8580e1cbbb03
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-10-31 17:52:29 +1100

    zoom in if click on nodes

--- canvas/js/drawing.js
+++ canvas/js/drawing.js
@@ -1,9 +1,10 @@
 var width = $(window).width(), height = $(window).height();
-var legend_margin = 200;
+var margin = 5, legend_margin = 200, text_margin = 15;
 var draw = SVG('drawing').size(width, height);
-var bbox = [0, 0, 0, 0] // (x_min, x_max, y_min, y_max)
+    draw.rect(width, height).fill("#fff").click(function() { zoom_out() });
+    draw.viewbox(0,0,width,height);
 var nsize = 6, nsizeb = 10;
-
+var bbox = [width,-width,height,-height,0,0]; // (x_min, x_max, y_min, y_max, x_center, y_center)
 var colors = ["#065143", "#129490",
   "#70B77E", "#E0A890", "#CE1483", "#fe4a49",
   "#A2DDE1", "#FCAB10", "#ff9234", "#fd0054",
@@ -11,6 +12,7 @@ var colors = ["#065143", "#129490",
 var glist = [];
 var every_nodes = {};
 var every_nodes_t = {};
+var zoomed = false;
 
 var filename = GetURLParameter("input");
 console.log(filename)
@@ -19,31 +21,39 @@ $.getJSON("http://127.0.0.1:8080/data/"+filename, function( data ) {
     var groups = new Set();
     for (var key in data) {
       // calculating boundary box
-      bbox[0] = Math.min(bbox[0], data[key][0])
-      bbox[1] = Math.max(bbox[1], data[key][0])
-      bbox[2] = Math.min(bbox[2], data[key][1])
-      bbox[3] = Math.max(bbox[3], data[key][1])
+      bbox[0] = Math.min(bbox[0], data[key][0]-margin)
+      bbox[1] = Math.max(bbox[1], data[key][0]+margin)
+      bbox[2] = Math.min(bbox[2], data[key][1]-margin)
+      bbox[3] = Math.max(bbox[3], data[key][1]+margin)
       // save names of nodes
       var name = key.split("_");
       groups.add(name[0]);
       every_nodes[name[0]] = [];
       every_nodes_t[name[0]] = [];
     }
+    bbox[4] = (bbox[0]+bbox[1])/2;
+    bbox[5] = (bbox[2]+bbox[3])/2;
 
     glist = Array.from(groups); // list of names
-    var xd = bbox[1]-bbox[0]+10,
-        yd = bbox[3]-bbox[2]+10;
-    var xs = 1+(width-legend_margin)/xd, ys = 1+height/yd;
-
+    var xd = bbox[1]-bbox[0],
+        yd = bbox[3]-bbox[2];
+    var xs = (width-legend_margin)/xd, ys = height/yd;
+    console.log("original", xd, yd, xs, ys);
     // draw circle for each point
     for (var key in data) {
       var name = key.split("_");
       var gname = name[0], year = name[1];
+      var newx = (data[key][0]-bbox[4])*xs+(width-legend_margin)/2,
+          newy = (data[key][1]-bbox[5])*ys+height/2;
+      // console.log(data[key][0], data[key][1], newx, newy);
       var circle = draw.circle(nsize*2).id(key)
-          .center(data[key][0]*xs+(width-legend_margin)/2, data[key][1]*ys+height/2+30)
+          .attr("ox", data[key][0]).attr("oy", data[key][1])
+          .attr("px", newx).attr("py", newy)
+          .center(newx, newy)
           .fill(colors[glist.indexOf(gname)]);
       circle.mouseover(function() { highlight_node(this.node.id) });
       circle.mouseout(function() { reset_highlight() });
+      circle.click(function() { zoom_in_node(this.node.id) });
       every_nodes[gname].push(circle);
     }
 
@@ -51,8 +61,10 @@ $.getJSON("http://127.0.0.1:8080/data/"+filename, function( data ) {
     for (var key in data) {
       var name = key.split("_");
       var gname = name[0], year = name[1];
+      var newx = (data[key][0]-bbox[4])*xs+(width-legend_margin)/2,
+          newy = (data[key][1]-bbox[5])*ys+height/2;
       var c_text = draw.text(year).id(key).fill("#000")
-          .move(data[key][0]*xs+(width-legend_margin)/2+10, data[key][1]*ys+height/2+30)
+          .attr("x", newx+text_margin).attr("y", newy)
           .attr("visibility", "hidden");
       every_nodes_t[gname].push(c_text);
     }
@@ -74,9 +86,57 @@ $.getJSON("http://127.0.0.1:8080/data/"+filename, function( data ) {
 
 });
 
+function zoom_out() {
+  if (!zoomed) return;
+  zoomed = false;
+  reset_highlight();
+  draw.animate(100).viewbox(0, 0, width, height);
+}
+
+function calculate_bbox(data) {
+  var box = [width,-width,height,-height,0,0];
+  for (var key in data) {
+    // calculating boundary box
+    box[0] = Math.min(box[0], data[key].node.getAttribute("px"));
+    box[1] = Math.max(box[1], data[key].node.getAttribute("px"));
+    box[2] = Math.min(box[2], data[key].node.getAttribute("py"));
+    box[3] = Math.max(box[3], data[key].node.getAttribute("py"));
+  }
+  // console.log("calculate_canvas", box);
+  box[4] = (box[0]+box[1])/2;
+  box[5] = (box[2]+box[3])/2;
+  return box
+}
+
+function zoom_in_node(nid) {
+  zoomed = true;
+  dim_every_nodes(0.2);
+  var gname = nid.split("_")[0];
+  var nbox = calculate_bbox(every_nodes[gname]);
+  var xd = 50+nbox[1]-nbox[0], yd = 50+nbox[3]-nbox[2];
+  var newwidth = 10, newheight = 10;
+  if (xd > yd) {
+    newwidth += xd, newheight += height*xd/width;
+  } else {
+    newwidth += width*yd/height, newheight += yd;
+  }
+  console.log(draw.bbox());
+  console.log(xd, yd, newwidth, newheight);
+  console.log(nbox);
+  // draw.animate(100).viewbox(nbox[4]-newwidth/2, nbox[5]-newheight/2, nbox[4]+newwidth/2, nbox[5]+newheight/2);
+  draw.animate(100).viewbox(nbox[4]-xd/2, nbox[5]-yd/2, nbox[4]+xd/2, nbox[5]+yd/2);
+
+  for (var e in every_nodes[gname]) {
+    every_nodes[gname][e].attr("r", nsize).attr("opacity", 1)
+        .fill(colors[glist.indexOf(gname)]);
+    every_nodes_t[gname][e].attr("visibility", "visible");
+  }
+}
+
 function highlight_node(nid) {
+  if (zoomed) return;
   // console.log("highlight_node", nid);
-  dim_every_nodes();
+  dim_every_nodes(0.6);
   var members = SVG.select("#"+nid).members;
   for (var n in members) {
     if (members[n].type == "circle") { members[n].attr("r", nsizeb).attr("opacity", 1); }
@@ -85,8 +145,9 @@ function highlight_node(nid) {
 }
 
 function highlight_group(gname) {
+  if (zoomed) return;
   // console.log("highlight_group", gname);
-  dim_every_nodes();
+  dim_every_nodes(0.6);
   for (var e in every_nodes[gname]) {
     every_nodes[gname][e].attr("r", nsizeb).attr("opacity", 1)
         .fill(colors[glist.indexOf(gname)]);
@@ -94,24 +155,26 @@ function highlight_group(gname) {
   }
 }
 
-function dim_every_nodes() {
+function dim_every_nodes(opct) {
   for (var gname in every_nodes) {
     for (var e in every_nodes[gname]) {
-      every_nodes[gname][e].attr("opacity", 0.6);
+      every_nodes[gname][e].attr("opacity", opct);
     }
   }
 }
 
 function reset_highlight() {
+  if (zoomed) return;
   for (var gname in every_nodes) {
     for (var e in every_nodes[gname]) {
+      var tmp = every_nodes[gname][e].node;
+      var newx = tmp.getAttribute("px"), newy = tmp.getAttribute("py");
       every_nodes[gname][e].attr("r", nsize)
+          .attr("cx", newx).attr("cy", newy)
           .attr("opacity", 1)
           .fill(colors[glist.indexOf(gname)]);
-    }
-    for (var e in every_nodes_t[gname]) {
-      var y = every_nodes_t[gname][e].id();
-      every_nodes_t[gname][e].text(y.split("_")[1]).attr("visibility", "hidden");
+      every_nodes_t[gname][e].text(tmp.id.split("_")[1]).attr("visibility", "hidden")
+          .attr("x", newx+text_margin).attr("y", newy)
     }
   }
 }
