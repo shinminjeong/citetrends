14c3ffec5e34df1fc253d66f99af90ac2f33d393
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-11-20 12:27:15 +1100

    change drawing order, code clean up

--- canvas/js/drawing.js
+++ canvas/js/drawing.js
@@ -3,6 +3,12 @@ var margin = 5, legend_margin = 200, text_margin = 15;
 var draw = SVG('drawing').size(width, height);
     draw.rect(width, height).fill("#fff").dblclick(function() { zoom_out() });
     draw.viewbox(0,0,width,height);
+// this decides layer order
+var draw_edge = draw.group();
+var draw_node = draw.group();
+var draw_text = draw.group();
+var draw_legend = draw.group();
+
 var nsize = 6, nsizeb = 10;
 var bbox = [width,-width,height,-height,0,0]; // (x_min, x_max, y_min, y_max, x_center, y_center)
 var colors = ["#065143", "#129490",
@@ -19,7 +25,7 @@ var zoomed = false;
 var filename = GetURLParameter("input");
 console.log(filename)
 
-$.getJSON("http://127.0.0.1:8080/data/"+filename, function( data ) {
+$.getJSON("http://127.0.0.1:8080/data/conf_trends/"+filename, function( data ) {
     var groups = new Set();
     for (var key in data) {
       // calculating boundary box
@@ -50,7 +56,7 @@ $.getJSON("http://127.0.0.1:8080/data/"+filename, function( data ) {
       var newx = (data[key][0]-bbox[4])*xs+(width-legend_margin)/2,
           newy = (data[key][1]-bbox[5])*ys+height/2;
       // console.log(data[key][0], data[key][1], newx, newy);
-      var circle = draw.circle(nsize*2).id(key)
+      var circle = draw_node.circle(nsize*2).id(key)
           .attr("ox", data[key][0]).attr("oy", data[key][1])
           .attr("px", newx).attr("py", newy)
           .center(newx, newy)
@@ -67,17 +73,19 @@ $.getJSON("http://127.0.0.1:8080/data/"+filename, function( data ) {
       var gname = name[0], year = name[1];
       var newx = (data[key][0]-bbox[4])*xs+(width-legend_margin)/2,
           newy = (data[key][1]-bbox[5])*ys+height/2;
-      var c_text = draw.text(year).id(key).fill("#000")
+      var c_text = draw_text.text(year).id(key).fill("#000")
           .attr("x", newx+text_margin).attr("y", newy)
+          .attr("stroke", 1)
+          .attr("stroke-color", "white")
           .attr("visibility", "hidden");
       every_nodes_t[gname].push(c_text);
     }
 
     // draw legends
     for (var gid in glist) {
-      var legend_rect = draw.rect(20,20).id(gid)
+      var legend_rect = draw_legend.rect(20,20).id(gid)
           .fill(colors[gid%colors.length]).move(width-legend_margin, gid*25+20);
-      var legend_text = draw.text(glist[gid]).id(gid)
+      var legend_text = draw_legend.text(glist[gid]).id(gid)
           .move(width-legend_margin+30, gid*25+20)
           .fill("#000");
 
@@ -114,9 +122,12 @@ function calculate_bbox(data) {
 }
 
 function zoom_in_node(nid) {
+  if (zoomed) return;
   zoomed = true;
   dim_every_nodes(0.2);
   var gname = nid.split("_")[0];
+
+  // calculate bbox region to zome in
   var nbox = calculate_bbox(every_nodes[gname]);
   var xd = 50+nbox[1]-nbox[0], yd = 50+nbox[3]-nbox[2];
   var newwidth = 100, newheight = 100; // added margin
@@ -134,6 +145,7 @@ function zoom_in_node(nid) {
     zoom: Math.min(width/newwidth, height/newheight)
   });
 
+  // chaneg node color and draw edges
   for (var e in every_nodes[gname]) {
     var year_n = every_nodes[gname][e].node.id.split("_")[1];
     if (year_n%5 == 0) {
@@ -141,7 +153,7 @@ function zoom_in_node(nid) {
           .fill(colors[glist.indexOf(gname)%colors.length]);
     } else {
       every_nodes[gname][e].attr("r", nsize).attr("opacity", 1)
-          .fill("transparent").attr("stroke", colors[glist.indexOf(gname)%colors.length]);
+          .fill("white").attr("stroke", colors[glist.indexOf(gname)%colors.length]);
     }
     var year_t = every_nodes_t[gname][e].node.id.split("_")[1];
     if (year_t%5 == 0) {
@@ -177,7 +189,7 @@ function draw_edges_group(gname) {
     var cur = SVG.get("#"+gname+"_"+yearlist[y]);
     if (cur) {
       if (prv) {
-        var path = draw.line(prv.node.getAttribute("cx"), prv.node.getAttribute("cy"),
+        var path = draw_edge.line(prv.node.getAttribute("cx"), prv.node.getAttribute("cy"),
                           cur.node.getAttribute("cx"), cur.node.getAttribute("cy"))
                        .stroke(colors[glist.indexOf(gname)%colors.length]);
         every_edges[gname].push(path);
