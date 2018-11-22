38aa124f04a3405801f5868efbb742eddeb20310
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-11-22 11:49:48 +1100

    normalize by num paper each year

--- ent2Vec/utils.py
+++ ent2Vec/utils.py
@@ -164,13 +164,19 @@ def save_plot_result(filename, dictdata):
         json.dump(dictdata, outfile)
 
 
-def get_set_of_venues_by_year(res):
+def get_set_of_venues_by_year(cname, res):
     references = []
     for p, r in res.items():
         if "References" in r and "Year" in r:
             references.append((r["Year"], r["References"]))
     venues = {}
     for y, ref in references:
+        # count numpaper each year
+        if y in name_id_pairs[cname]["numpaper"]:
+            name_id_pairs[cname]["numpaper"][y] += 1
+        else:
+            name_id_pairs[cname]["numpaper"][y] = 1
+
         if y not in venues:
             venues[y] = []
         venues[y].extend(ref)
@@ -182,13 +188,15 @@ def aggr_venues(data):
         venues.extend(ref)
     return venues
 
-def get_vector(name, bov, author_venue, norm=False):
+def get_vector(cname, bov, author_venue, year=0, norm=False):
     c = Counter(author_venue)
     author_arr = [[float(c[b]) for b in bov]]
+    res_arr = np.array(author_arr)
+    if year != 0:
+        res_arr /= name_id_pairs[cname]["numpaper"][year]
     if norm:
-        return np.array(author_arr)/name_id_pairs[name]["numpaper"]
-    else:
-        return np.array(author_arr)
+        res_arr /= name_id_pairs[cname]["totalpaper"]
+    return res_arr
 
 name_id_pairs = {
     # "steve_blackburn": {"id": 2146610949, "type":"author"},
@@ -197,20 +205,20 @@ name_id_pairs = {
     # "julian_dolby": {"id": 2780161863, "type":"author"},
     # "perry_cheng": {"id": 2140441920, "type":"author"},
     # "julian_mcauley": {"id": 2041520510, "type":"author"},
-    "ICWSM": {"id": 1124713781, "type":"conf"},
-    "POPL": {"id": 1160032607, "type":"conf"},
-    "PLDI": {"id": 1127352206, "type":"conf"},
-    "WSDM": {"id": 1120384002, "type":"conf"},
-    "CIKM": {"id": 1194094125, "type":"conf"},
-    "ISCA": {"id": 1131341566, "type":"conf"},
-    "MICRO": {"id": 1150919317, "type":"conf"},
-    "ASPLOS": {"id": 1174091362, "type":"conf"},
-    "OSDI": {"id": 1185109434, "type":"conf"},
-    "ICML": {"id": 1180662882, "type":"conf"},
-    "NIPS": {"id": 1127325140, "type":"conf"},
-    "OOPSLA": {"id": 1138732554, "type":"conf"},
-    "WWW": {"id": 1135342153, "type":"conf"},
-    "AAAI": {"id": 1184914352, "type":"conf"},
+    "ICWSM": {"id": 1124713781, "type":"conf", "numpaper":{}},
+    "POPL": {"id": 1160032607, "type":"conf", "numpaper":{}},
+    "PLDI": {"id": 1127352206, "type":"conf", "numpaper":{}},
+    "WSDM": {"id": 1120384002, "type":"conf", "numpaper":{}},
+    "CIKM": {"id": 1194094125, "type":"conf", "numpaper":{}},
+    "ISCA": {"id": 1131341566, "type":"conf", "numpaper":{}},
+    "MICRO": {"id": 1150919317, "type":"conf", "numpaper":{}},
+    "ASPLOS": {"id": 1174091362, "type":"conf", "numpaper":{}},
+    "OSDI": {"id": 1185109434, "type":"conf", "numpaper":{}},
+    "ICML": {"id": 1180662882, "type":"conf", "numpaper":{}},
+    "NIPS": {"id": 1127325140, "type":"conf", "numpaper":{}},
+    "OOPSLA": {"id": 1138732554, "type":"conf", "numpaper":{}},
+    "WWW": {"id": 1135342153, "type":"conf", "numpaper":{}},
+    "AAAI": {"id": 1184914352, "type":"conf", "numpaper":{}},
 }
 
 def generate_data():
@@ -219,13 +227,13 @@ def generate_data():
         save_as_file(cname, data)
 
 
-def generate_bov():
+def generate_bov_year():
     conf_vectors = {}
     for cname in name_id_pairs.keys():
         with open("../data/{}.json".format(cname), "r") as infile:
             data = json.load(infile)
-            name_id_pairs[cname]["numpaper"] = len(data)
-            conf_vectors[cname] = get_set_of_venues_by_year(data)
+            name_id_pairs[cname]["totalpaper"] = len(data)
+            conf_vectors[cname] = get_set_of_venues_by_year(cname, data)
     return conf_vectors
 
 def print_cos_similarity(vec):
@@ -233,7 +241,7 @@ def print_cos_similarity(vec):
         print(x, y, cosine_similarity(vec[x], vec[y]))
 
 
-def reduce_vec_pca(vec):
+def reduce_vec_pca(vec, number_of_venues):
     pca = PCA(n_components=2)
     X = np.zeros((len(vec),number_of_venues))
     for i, v in enumerate(vec.values()):
@@ -247,7 +255,7 @@ def reduce_vec_pca(vec):
         result[k] = X_pca[i].tolist()
     return result
 
-def reduce_vec_tsne(vec, p):
+def reduce_vec_tsne(vec, p, number_of_venues):
     tsne = TSNE(n_components=2, perplexity=p)
     X = np.zeros((len(vec),number_of_venues))
     for i, v in enumerate(vec.values()):
@@ -261,43 +269,87 @@ def reduce_vec_tsne(vec, p):
     return result
 
 
-if __name__ == '__main__':
-    # generate_data()
+def download_data_save_as_json():
+    return generate_data()
 
-    data = generate_bov()
+def generate_year_trends_plots():
+    data = generate_bov_year()
 
+    ## calculate total bag of venue
     bag_of_venues = set()
     sorted_list_bov = list()
     for name, y_venues in data.items():
         venues = aggr_venues(y_venues)
-        print("{}: len(papers)={}, len(venues)={}, len(set(venues))={}".format(name, name_id_pairs[name]["numpaper"], len(venues), len(set(venues))))
+        print("{}: len(papers)={}, len(venues)={}, len(set(venues))={}".format(name, name_id_pairs[name]["totalpaper"], len(venues), len(set(venues))))
+        print(name_id_pairs[name]["numpaper"])
         [bag_of_venues.add(v) for v in venues]
     print("Total # of venues = ", len(bag_of_venues))
     sorted_list_bov = list(bag_of_venues)
     number_of_venues = len(sorted_list_bov)
 
-    ## generate bov for each entity
-    vec = {}
-    avg_vec = {}
-    print(data.keys())
-    for name, y_venues in data.items():
-        vec[name] = get_vector(name, sorted_list_bov, aggr_venues(y_venues))
-        avg_vec[name] = get_vector(name, sorted_list_bov, aggr_venues(y_venues), True)
-    # print_cos_similarity(vec)
-    # print(reduce_vec_dim(vec))
-
     ## generate year_bov for each entity
     vec = {}
     for name, y_venues in data.items():
         for y, ref in y_venues.items():
-            vec["{}_{}".format(name, y)] = get_vector(name, sorted_list_bov, ref)
+            vec["{}_{}".format(name, y)] = get_vector(name, sorted_list_bov, ref, year=y, norm=True)
     # print_cos_similarity(vec)
-    reduce_vec = reduce_vec_pca(vec)
+    reduce_and_save(vec, number_of_venues, "year")
+
+
+def reduce_and_save(vec, number_of_venues, tag):
+    reduce_vec = reduce_vec_pca(vec, number_of_venues)
     # print(reduce_vec)
-    save_plot_result("pca_vec", reduce_vec)
+    save_plot_result("pca_{}".format(tag), reduce_vec)
 
     for i in [1,2,3]:
         for p in [5, 10, 20, 40, 80]:
-            reduce_t_vec = reduce_vec_tsne(vec, p)
+            reduce_t_vec = reduce_vec_tsne(vec, p, number_of_venues)
             # print(reduce_vec)
-            save_plot_result("tsne_vec_{}_{}".format(i, p), reduce_t_vec)
+            save_plot_result("tsne_{}_{}_{}".format(tag, i, p), reduce_t_vec)
+
+
+def generate_bov_paper():
+    paper_vectors = {}
+    for cname in name_id_pairs.keys():
+        with open("../data/{}.json".format(cname), "r") as infile:
+            data = json.load(infile)
+            name_id_pairs[cname]["totalpaper"] = len(data)
+            for p, value in data.items():
+                paper_vectors["{}_{}_{}".format(cname, value["Year"], p)] = value["References"]
+    return paper_vectors
+
+def generate_indv_paper_plots():
+    data = generate_bov_paper()
+
+    bag_of_venues = set()
+    sorted_list_bov = list()
+    venues = aggr_venues(data)
+    print("len(papers)={}, len(venues)={}, len(set(venues))={}".format(len(data), len(venues), len(set(venues))))
+    [bag_of_venues.add(v) for v in venues]
+    print("Total # of venues = ", len(bag_of_venues))
+    sorted_list_bov = list(bag_of_venues)
+    number_of_venues = len(sorted_list_bov)
+
+    ## generate year_bov for each paper
+    vec = {}
+    for paper, ref_venuew in data.items():
+        if len(ref_venuew) == 0:
+            continue
+        cname, y, pid = paper.split("_")
+        # print(cname, y, pid, len(ref_venuew))
+        vec[paper] = get_vector(cname, sorted_list_bov, ref_venuew, norm=True)
+
+    ## generate average vectors by years
+    year_data = generate_bov_year()
+    for cname, y_venues in year_data.items():
+        for y, ref in y_venues.items():
+            vec["{}_{}_average".format(cname, y)] = get_vector(cname, sorted_list_bov, ref, year=y, norm=True)
+
+    # print(vec)
+    reduce_and_save(vec, number_of_venues, "indv")
+
+
+if __name__ == '__main__':
+    # download_data_save_as_json()
+    generate_year_trends_plots()
+    # generate_indv_paper_plots()
